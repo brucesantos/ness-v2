77cbc9ca71528dadb4a2bf21df07d66016889637
Leonardo Falotico <leonardo.falotico@beautyq.com.br>
2019-08-15 22:49:43 -0300

    Update Home

--- common/header.php
+++ common/header.php
@@ -1,51 +1,21 @@
-<a class="open_menu color-main bg-light radius_full"><i class="fas fa-bars lh-40"></i></a>
-
-<div class="navigation_mobile bg-light type2">
-  <a class="close_menu color-main"><i class="fas fa-times"></i></a>
-  
-  <div class="px-40 pt-60 pb-60 text-center inner">
-    <div><a href="#" class="f-heading f-22 link color-main mb-20">Home</a></div>
-    <div><a href="#" class="f-heading f-22 link color-main mb-20">Popular</a></div>
-    <div><a href="#" class="f-heading f-22 link color-main mb-20">Recent</a></div>
-    <div><a href="#" class="f-heading f-22 link color-main mb-20">Featured</a></div>
-    <div><a href="#" class="f-heading f-22 link color-main mb-20">Designers</a></div>
-        
-    <div><a href="#" class="f-heading f-22 link color-main mb-20">Team</a></div>
-    <div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">Help</a></div>
-    <div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">F.A.Q.</a></div>
-    <div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">Support</a></div>
-    <div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">About Us</a></div>
-    <div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">Blog</a></div>
-    <div><a href="#" class="f-12 link color-main op-7 text-uppercase sp-20 mb-20">Careers</a></div>
-
-    <div class="socials mt-60">
-      <a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-twitter"></i></a>
-      <a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-facebook"></i></a>
-      <a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-dribbble"></i></a>
-      <a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-instagram"></i></a>
-      <a href="#" target="_blank" class="link color-main f-18 mx-10"><i class="fab fa-behance"></i></a>
-    </div>
-  </div>
-</div>
-
-
-<header class="pt-250 pb-250 bg-dark header_2">
-  <nav class="header_menu_2 transparent pt-30 pb-30">
+<header>
+  <nav>
     <div class="container px-xl-0">
-      <div class="row justify-content-between align-items-baseline">
+      <div class="row">
         <div class="col-md-4">
-          <div class="logo color-white">
+          <div class="logo">
             <img class="logo" height="50px" src="images/ness-logo-website-header.svg">
+            <img class="logo-sticky" height="50px" src="images/ness-logo-website.svg">
           </div>
         </div>
         
 
-        <div class="col-md-6 d-flex justify-content-end align-items-center">
-          <ul class="nav justify-content-end">
-            <li class="nav-item"><a class="link color-white f-16 mx-25" href="#sobre">Sobre</a></li>
+        <div class="col-md-8">
+          <ul class="nav">
+            <li class="nav-item"><a class="link f-16 mx-25" href="#sobre">Sobre</a></li>
 
             <li class="nav-item">
-              <a class="dropdown-toggle link color-white f-16 mx-25" href="" id="navbardrop" data-toggle="dropdown">Verticais</a>
+              <a class="dropdown-toggle link f-16 mx-25" href="" id="navbardrop" data-toggle="dropdown">Verticais</a>
               <div class="dropdown-menu">
                 <a class="dropdown-item f-16" href="../health.html">Health</a>
                 <a class="dropdown-item f-16" href="../education.html">Education</a>
@@ -55,39 +25,13 @@
               </div>
             </li>
 
-            <li class="nav-item"><a class="link color-white f-16 mx-25" href="#clientes">Clientes</a></li>
-            <li class="nav-item"><a class="link color-white f-16 mx-25" href="#vagas">Vagas</a></li>
-            <li class="nav-item"><a class="link color-white f-16 mx-25" href="#contato">Contato</a></li>
+            <li class="nav-item"><a class="link f-16 mx-25" href="#clientes">Clientes</a></li>
+            <li class="nav-item"><a class="link f-16 mx-25" href="#vagas">Vagas</a></li>
+            <li class="nav-item"><a class="link f-16 mx-25" href="#contato">Contato</a></li>
           </ul>
 
         </div>
       </div>
     </div>
   </nav>
-
-  <div class="container">
-    <div class="mb-3 logo color-white d-block d-xl-none text-center logo_mobile">Startup 3</div>
-    <h1 class="big color-white text-center" data-aos-duration="700" data-aos="fade-down" data-aos-delay="0">Let's make design <br />fast and easy</h1>
-    <!-- <div class="mw-620 mx-auto mt-35 f-22 color-white op-7 text-center text-adaptive">
-      <span data-aos-duration="700" data-aos="fade-down" data-aos-delay="350">
-        The most important part of the Startup is the samples. 
-        The samples form a set of 25 usable pages you can use 
-        as is or you can add new blocks from UI Kit.			
-      </span>
-    </div> -->
-    
-    <!-- <form class="row align-items-center justify-content-center no-gutters mt-70" data-aos-duration="700" data-aos="fade-down" data-aos-delay="700" method="POST" action="form-handler.php">
-      <div class="col-xl-5 col-lg-6 col-md-8 col-sm-9 d-flex flex-wrap justify-content-center justify-content-md-between holder">
-        <input type="email" name="email" placeholder="Your email" class="mw-320 input lg border-transparent-white focus-white color-white placeholder-transparent-white f-20" required />
-        <button class="btn action-1 lg">Send</button>
-      </div>
-    </form> -->
-    
-    <!-- <div class="color-white op-3 text-center mt-35">
-      <span data-aos-duration="700" data-aos="fade-down" data-aos-delay="1050">			
-      By signing up, you agree to the 
-      <a href="#" class="link color-white">Terms of Service</a>			</span>			
-    </div> -->
-      
-  </div>
 </header>
