8586aad1cfb0787ed18340c49ce9cb83d5975db3
Bruno Santos <bruce.santos@gmail.com>
2019-10-06 22:44:20 -0300

    efeito de entrada do logo

--- js/iconess-animado.js
+++ js/iconess-animado.js
@@ -1,19 +1,39 @@
 $( document ).ready(function() {
 
-  var $circulo = jQuery('.circulo');
+  var $circulo = $('.circulo');
 
-  TweenMax.staggerFromTo($circulo, 2, {
-    repeat: -1,
-    scale: .9,
-    ease: Elastic.easeOut.config(1, 0.5),
-    transformOrigin: "50% 50%"
+  TweenMax.staggerFrom($circulo, 1, {
+    delay: .3,
+    scale: 0,
+    transformOrigin: "50% 50%",
+    ease: Back.easeOut.config(1),
   },
-  {
-    repeat: -1,
+  .02);
+
+  // TweenMax.from($('#curva-n'), 3, {
+  //   delay: 1.5,
+  //   scale: 1,
+  //   transformOrigin: "50% 50%",
+  //   ease: Elastic.easeOut.config(1, 0.5),
+  // },
+  // .02);
+
+  TweenMax.from($('#curva-n'), .3, {
+    delay: 1.3,
+    opacity: 0,
+    ease: Power2.easeOut
+  });
+
+  TweenMax.staggerTo($circulo, 3, {
+    delay: 4,
     scale: 1.1,
+    transformOrigin: "50% 50%",
     ease: Elastic.easeOut.config(1, 0.5),
-    transformOrigin: "50% 50%"
+    yoyo:true,
+    yoyoEase:true,
+    repeat: -1,
+    // repeatDelay: 1.8,
   },
-  .01);
+  .02);
 
 });
